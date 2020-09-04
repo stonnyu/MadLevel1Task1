b2ad26620a7d4b3cb2a9644e545c77e75b801f8f
stonnyu <mrtonnysu@gmail.com>
2020-09-04 11:10:47 +0200

    Created layout of dice number guess game.

---
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -0,0 +1,21 @@
+package com.example.madlevel1task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
+
+class HigherLowerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_higher_lower)
+    }
+
+    private fun updateUI() {
+
+    }
+}
